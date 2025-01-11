b5283da8d1e1ecb6c2e4e91eb479c82094f9e63c
razikdontcare <gamersia4@gmail.com>
2025-01-11 15:16:02 +0800

    feat: enhance Navbar with login and showcase buttons, adjust layout and styles

--- components/client/navbar.tsx
+++ components/client/navbar.tsx
@@ -9,36 +9,46 @@ import {
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import { Button } from "@/components/ui/button";
 
 const links = [
   { name: "Home", href: "/" },
   { name: "About", href: "/about" },
   { name: "Contact", href: "/contact" },
-  { name: "Showcase", href: "/showcase" },
 ];
 
 export default function Navbar() {
   return (
     <>
       <div className="flex w-full items-center justify-center">
-        <div className="container mx-auto flex items-center justify-between p-5">
+        <div className="container mx-auto flex items-center justify-between p-3">
           <div className="flex items-center justify-center gap-3">
-            <Image src={logo} alt="Informatika UNUD" className="size-12" />
-            <NavigationMenu>
-              <NavigationMenuList>
-                {links.map((link, i) => (
-                  <NavigationMenuItem key={i}>
-                    <Link href={link.href} legacyBehavior passHref>
-                      <NavigationMenuLink
-                        className={navigationMenuTriggerStyle()}
-                      >
-                        {link.name}
-                      </NavigationMenuLink>
-                    </Link>
-                  </NavigationMenuItem>
-                ))}
-              </NavigationMenuList>
-            </NavigationMenu>
+            <Image src={logo} alt="Informatika UNUD" className="size-10" />
+            <div className="flex items-center justify-center">
+              <NavigationMenu>
+                <NavigationMenuList>
+                  {links.map((link, i) => (
+                    <NavigationMenuItem key={i}>
+                      <Link href={link.href} legacyBehavior passHref>
+                        <NavigationMenuLink
+                          className={navigationMenuTriggerStyle()}
+                        >
+                          {link.name}
+                        </NavigationMenuLink>
+                      </Link>
+                    </NavigationMenuItem>
+                  ))}
+                </NavigationMenuList>
+              </NavigationMenu>
+            </div>
+          </div>
+          <div className="flex items-center justify-center gap-3">
+            <Button asChild variant={"outline"}>
+              <Link href="/login">Login</Link>
+            </Button>
+            <Button asChild>
+              <Link href="/showcase">Showcase</Link>
+            </Button>
           </div>
         </div>
       </div>
