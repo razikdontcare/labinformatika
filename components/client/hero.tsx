ed80793bff9c9c9b4582cebecdb913aeaf94df5d
razikdontcare <gamersia4@gmail.com>
2025-01-16 16:38:47 +0800

    feat: update About and Hero components for responsive illustration visibility and layout adjustments

--- components/client/hero.tsx
+++ components/client/hero.tsx
@@ -17,21 +17,25 @@ export default function Hero() {
         className="relative flex min-h-screen w-full flex-col overflow-clip"
         role="banner"
       >
-        <PythonIllustration className="fixed -left-10 top-10 size-80 rotate-6 opacity-50" />
-        <ServerIllustration className="fixed -right-10 top-10 size-80 -rotate-6 opacity-50" />
-        <WordpressIllustration className="fixed -bottom-11 left-12 size-64 -rotate-2 opacity-50" />
-        <ProgrammingIllustration className="fixed -bottom-11 right-0 size-80 -rotate-2 opacity-50" />
+        <PythonIllustration className="-left-10 top-10 hidden size-80 rotate-6 opacity-50 md:fixed md:flex" />
+        <ServerIllustration className="-right-10 top-10 hidden size-80 -rotate-6 opacity-50 md:fixed md:flex" />
+        <WordpressIllustration className="-bottom-11 left-12 hidden size-64 -rotate-2 opacity-50 md:fixed md:flex" />
+        <ProgrammingIllustration className="-bottom-11 right-0 hidden size-80 -rotate-2 opacity-50 md:fixed md:flex" />
         <Navbar />
-        <div className="flex flex-1 flex-col items-center justify-center bg-neutral-50">
+        <div className="flex flex-1 flex-col items-center justify-center bg-neutral-50 p-5 md:p-0">
           <div className="container z-10 mx-auto flex flex-col items-center justify-center gap-5">
             <div className="flex items-center justify-center rounded-xl border bg-gradient-to-tl from-neutral-300 from-5% to-white p-2 shadow-xl">
-              <Image src={logo} alt="Informatika UNUD" className="size-14" />
+              <Image
+                src={logo}
+                alt="Informatika UNUD"
+                className="size-10 md:size-14"
+              />
             </div>
-            <h1 className="flex flex-col items-center justify-center text-center text-7xl font-medium capitalize text-foreground">
+            <h1 className="flex flex-col items-center justify-center text-center text-3xl font-medium capitalize text-foreground md:text-7xl">
               <span>Inovasi Teknologi</span>
               <span className="text-neutral-500">Untuk Masa Depan</span>
             </h1>
-            <p className="text-lg">
+            <p className="text-center text-lg">
               Laboratorium Informatika Universitas Udayana
             </p>
             <Button asChild>
